63ef7976b69edf7ad722c66a3fa6a9fe2ebcec66
Ehsan Golpayegani <ehsan@cpan.org>
2012-07-22 13:17:20 +0000

    Import of EHSAN/Date-Jalali2-0.04 from CPAN.
    gitpan-cpan-distribution: Date-Jalali2
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         EHSAN/Date-Jalali2-0.04.tar.gz
    gitpan-cpan-author:       EHSAN
    gitpan-cpan-maturity:     released

--- Jalali2.pm
+++ Jalali2.pm
@@ -17,7 +17,7 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 my @g_days_in_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 my @j_days_in_month = (31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);
@@ -184,6 +184,7 @@ http://www.cpan.org/
 =head1 AUTHOR
 
 Ahmad Anvari <http://www.anvari.org/bio/>
+
 Redistributed by : Ehsan Golpayegani <http://www.golpayegani.com>
 
 =head1 COPYRIGHT AND LICENSE
--- t/1.t
+++ t/1.t
@@ -5,7 +5,7 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 9;
+use Test::More tests => 15;
 BEGIN { use_ok('Date::Jalali2') };
 
 #########################
